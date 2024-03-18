b34fe5165897a9b8da59fce1218f70f5fa21b2aa
David Alsh <alshdavid@gmail.com>
2024-03-19 10:15:00 +1100

    double async

--- a/src/main.rs
+++ a/src/main.rs
@@ -1,11 +1,21 @@
-fn main() {
+use std::{future::Future, pin::Pin};
+
+async fn main_async() {
     let exe_path = std::env::current_exe().unwrap();
     let exe_dir = exe_path.parent().unwrap();
     let lib_path = exe_dir.join("libb.so");
 
     unsafe {
         let lib = libloading::Library::new(&lib_path).unwrap();
-        let add_fn: libloading::Symbol<unsafe fn(usize, usize) -> usize> = lib.get(b"add").unwrap();
-        println!("{}", add_fn(1, 1));
+        let add_fn: libloading::Symbol<unsafe fn(usize, usize) -> Box<Pin<Box<dyn Future<Output = usize>>>>> = lib.get(b"add").unwrap();
+        println!("{}", add_fn(1, 1).await);
     }
+}
+
+pub fn main() {
+    tokio::runtime::Builder::new_multi_thread()
+      .enable_all()
+      .build()
+      .unwrap()
+      .block_on(main_async());
 }
--- b/src/lib.rs
+++ b/src/lib.rs
@@ -1,4 +1,18 @@
+use std::{future::Future, pin::Pin};
+
 #[no_mangle]
-pub extern fn add(left: usize, right: usize) -> usize {
-    left + right
+pub extern fn add(left: usize, right: usize) -> Box<Pin<Box<dyn Future<Output = usize>>>> {
+    return Box::new(Box::pin(async move {
+        tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(add_async(left, right))
+    }))
+}
+
+async fn add_async(left: usize, right: usize) -> usize {
+    tokio::task::spawn(async move {
+        left + right
+    }).await.unwrap()
 }
